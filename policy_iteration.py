b3ca234e0b8aa55b48e405c56eee84c51712570b
이수영 <ssoo6247@gmail.com>
2019-02-02 16:08:16 +0900

    Some bugs

--- policy_iteration.py
+++ policy_iteration.py
@@ -15,17 +15,18 @@ class PolicyIteration:
         self.discount_factor = 0.9
 
     # Policy evaluation that computes next value function by Bellman expectation equation.
-    def policy_evaluation(self)
+    def policy_evaluation(self):
         # Initialize next value function.
         next_value_table = [[0.00] * self.env.width for _ in  range(self.env.height)]
 
         for state in self.env.get_all_states():
             value = 0.0
-            if state = [2, 2]:
+            # End state's value function = 0
+            if state == [2, 2]:
                 next_value_table[state[0]][state[1]] = 0.0
                 continue
-            
-            for action in selv.env.possible_actions:
+            # Bellman Expectation equation
+            for action in self.env.possible_actions:
                 next_state = self.env.state_after_action(state, action)
                 reward = self.env.get_reward(state, action)
                 next_value = self.get_value(next_state)
@@ -35,6 +36,7 @@ class PolicyIteration:
 
         self.value_table = next_value_table
 
+    # Greedy policy development for present value function
     def policy_improvement(self):
         next_policy = self.policy_table
         for state in self.env.get_all_states():
@@ -42,14 +44,17 @@ class PolicyIteration:
                 continue
             value = -99999
             max_index = []
+            # Initialization of returning policy
             result = [0.0, 0.0, 0.0, 0.0]
 
+            # Compute [reward + (discount_rate * next_state_value_function)] for all actions.
             for index, action in enumerate(self.env.possible_actions):
                 next_state = self.env.state_afteraction(state, action)
                 reward = self.env.get_reward(state, action)
                 next_value = self.get_value(next_state)
                 temp = reward + self.discount_factor * next_value
-
+                
+                # Extract action's index which has maximim reward.
                 if temp == value:
                     max_index.append(index)
                 elif temp > value:
@@ -57,14 +62,16 @@ class PolicyIteration:
                     max_index.clear()
                     max_index.append(index)
 
+            # Compute the probability of action.
             prob = 1 / len(max_index)
             for index in max_index:
                 result[index] = prob
 
-            next_policy[state[0][state[1]] = result
+            next_policy[state[0]][state[1]] = result
 
         self.policy_table = next_policy
 
+    # return action that follows policy in specific state.
     def get_action(self, state):
         random_pick = random.randrange(100) / 100
         policy = self.get_policy(state)
@@ -74,12 +81,13 @@ class PolicyIteration:
             if random_pick < policy_sum:
                 return index
 
+    # return policy follwed by state.
     def get_policy(self, state):
         if state == [2, 2]:
             return 0.0
         return self.policy_table[state[0]][state[1]]
 
-
+    # return the value of value function.
     def get_value(self, state):
         return round(self.value_table[state[0]][state[1]], 2)
     
